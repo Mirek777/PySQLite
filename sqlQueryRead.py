2d427cf8731a61f61b43a7e020bbafaf18c305be
Mirek777 <s-a-n-1@mail.ru>
2022-03-08 12:34:06 +0300

    3

--- sqlQueryRead.py
+++
@@ -1,7 +0,0 @@
-
-# SQL запрос на чтение всех записей из таблицы ЮЗЕРОВ
-select_users = "SELECT * FROM users"
-
-
-# SQL запрос на чтение всех записей из таблицы ЮЗЕРОВ
-select_posts = "SELECT * FROM posts"
